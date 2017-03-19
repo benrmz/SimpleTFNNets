eebc048b83bcefc0e5c88d0c71e645fb5215b335
benji <benji.b.ramirez@gmail.com>
2017-03-19 13:18:08 -0700

    or

--- or_net.py
+++ or_net.py
@@ -1,37 +1,31 @@
-# Benjamin Ramirez 3/18/2017
-# training neural networks to perform logical operations using tensorflow
-# objective is to demonstrate/learn tensorflow library
-
-# following network architecture from this site:
-# http://toritris.weebly.com/perceptron-2-logical-operations.html
-# and elementary intro to tensorflow here: https://medium.com/@jaschaephraim/elementary-neural-networks-with-tensorflow-c2593ad3d60b#.brk1wtlif
 
 import tensorflow as tf
 
-########### AND operation ###############
+######### OR operation ###############
 
+print ("----------------Beginning OR net-----------------")
 # Truth Table as vectors/matrices
-and_inputs = [
+or_inputs = [
     [1., 1.],
     [1., 0.],
     [0., 1.],
     [0., 0.]
 ]
 
-and_outputs = [
+or_outputs = [
     [1.],
-    [0.],
-    [0.],
+    [1.],
+    [1.],
     [0.]
 ]
 
-and_weights = tf.Variable(tf.random_normal([2,1]))
-and_bias = tf.Variable(tf.zeros([1]))
+or_weights = tf.Variable(tf.random_normal([2,1]))
+or_bias = tf.Variable(tf.zeros([1]))
 
 #acivation function, tanh = scaled version of sigmoid,
-hypothesis = tf.tanh(tf.add(tf.matmul( and_inputs, and_weights ) , and_bias))
+hypothesis = tf.tanh(tf.add(tf.matmul( or_inputs, or_weights ) , or_bias))
 
-error = tf.subtract(and_outputs, hypothesis)
+error = tf.subtract(or_outputs, hypothesis)
 cost = tf.reduce_mean(tf.square(error))
 # the learning rate, alpha
 alpha = 0.05
@@ -40,7 +34,7 @@ alpha = 0.05
 train_step = tf.train.GradientDescentOptimizer(alpha).minimize(cost)
 
 #max number of iterations if it fails to converge to target_error
-current_error, target_error = 100, 0.05
+current_error, target_error = 100, 0.001
 current_epoch, max_epochs = 0, 1500
 
 #now that we have sup up network, train it
@@ -53,14 +47,12 @@ with tf.Session() as sess:
         current_error, t = sess.run([cost, train_step])
         #check the progress of the training:
         if current_epoch % 100 == 0:
-            print()
+            print(" ")
             print( 'CHECKING PROGRESS')
             print('epoch: ', current_epoch)
             print('cost: ', sess.run(cost))
             # print ('hypothesis: ', sess.run(hypothesis, feed_dict=zip(and_inputs, and_outputs)))
-            print('Weights: ', sess.run(and_weights))
-            print('Bias: ', sess.run(and_bias))
+            print('Weights: ', sess.run(or_weights))
+            print('Bias: ', sess.run(or_bias))
 
     sess.close()
-
-
