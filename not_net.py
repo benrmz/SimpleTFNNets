5b070906963d45e93aa64af8c3741e93906e085d
benji <benji.b.ramirez@gmail.com>
2017-03-21 18:50:46 -0700

    not net

--- not_net.py
+++ not_net.py
@@ -1,31 +1,26 @@
 
 import tensorflow as tf
 
-######### OR operation ###############
 
-print ("----------------Beginning OR net-----------------")
-# Truth Table as vectors/matrices
-or_inputs = [
-    [1., 1.],
-    [1., 0.],
-    [0., 1.],
-    [0., 0.]
+print ("----------------Beginning NOT net-----------------")
+# Truth Table as vectors/matrices, simply output opposite of the single input
+not_inputs = [
+    [-1.],
+    [1.]
 ]
 
-or_outputs = [
+not_outputs = [
     [1.],
-    [1.],
-    [1.],
-    [0.]
+    [-1.],
 ]
 
-or_weights = tf.Variable(tf.random_normal([2,1]))
-# or_bias = tf.Variable(tf.zeros([1]))
+not_weights = tf.Variable(tf.random_normal([1,1]))
+not_bias = tf.Variable(tf.zeros([1]))
 
 #acivation function, tanh = scaled version of sigmoid,
-hypothesis = tf.tanh(tf.matmul( or_inputs, or_weights ))
+hypothesis = tf.tanh(tf.add(tf.matmul( not_inputs, not_weights ) , not_bias))
 
-error = tf.subtract(or_outputs, hypothesis)
+error = tf.subtract(not_outputs, hypothesis)
 cost = tf.reduce_mean(tf.square(error))
 # the learning rate, alpha
 alpha = 0.05
@@ -38,6 +33,7 @@ current_error, target_error = 100, 0.001
 current_epoch, max_epochs = 0, 1500
 
 #now that we have sup up network, train it
+#the weight should become negative, flipping the output
 with tf.Session() as sess:
     #initialze tf vars
     sess.run(tf.global_variables_initializer())
@@ -52,7 +48,7 @@ with tf.Session() as sess:
             print('epoch: ', current_epoch)
             print('cost: ', sess.run(cost))
             # print ('hypothesis: ', sess.run(hypothesis, feed_dict=zip(and_inputs, and_outputs)))
-            print('Weights: ', sess.run(or_weights))
-            # print('Bias: ', sess.run(or_bias))
+            print('Weights: ', sess.run(not_weights))
+            print('Bias: ', sess.run(not_bias))
 
     sess.close()
